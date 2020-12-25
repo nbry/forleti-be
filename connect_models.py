4a840b4f7c76e63c4f149ee9c3c61620204d46ab
nbry <nwbryant@umich.edu>
2020-12-24 16:09:26 -0800

    Connect app to flask_praetorian

--- connect_models.py
+++ connect_models.py
@@ -1,9 +1,16 @@
+from flask_praetorian import Praetorian
 from flask_sqlalchemy import SQLAlchemy
+from models.user import User
 
+guard = Praetorian()
 db = SQLAlchemy()
 
 
 def connect_db(app):
-    """ Connect this database to provided Flask app """
+    """
+    Connect Flask Praetorian to app.
+    Connect database to provided Flask app.
+    """
+    guard.init_app(app, User)
     db.app = app
     db.init_app(app)
