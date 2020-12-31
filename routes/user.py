04a7dbb72f5a62987a030369932da64e79adca85
nbry <nwbryant@umich.edu>
2020-12-31 14:46:29 -0800

    Implement token encoding for login and signup routes.

--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,5 @@
 """ Routes for user/auth-related tasks """
+import flask_praetorian as fp
 from flask import Blueprint, request, jsonify
 from connect_models import guard
 from models.user import User
@@ -13,7 +14,6 @@ def login():
     and returns a token.
     """
 
-    import ipdb;ipdb.set_trace()
     req = request.json
     user = guard.authenticate(
         req.get('username', None),
@@ -22,7 +22,8 @@ def login():
 
     if user:
         message = {
-            "message": f"Success! Logged in to {user.username}'s account"
+            "message": f"Success! Logged in to {user.username}'s account",
+            "token": guard.encode_jwt_token(user)
         }
         return jsonify(message)
     else:
@@ -33,7 +34,8 @@ def login():
 @user_api.route('/signup', methods=['POST'])
 def signup():
     """
-    Handles a request to sign user up for new account.
+    Handles a request to sign user up for new account. If successful,
+    returns a token.
     """
     req = request.json
     new_user = User.signup(
@@ -42,11 +44,39 @@ def signup():
         req.get('email', None)
     )
 
+    # TO BE IMPLEMENTED:
+    # 1. ERROR HANDLING FOR DUPLICATE ACCOUNT
+    # 2. ERROR HANDLING FOR DUPLICATE EMAIL
+
     if new_user:
         message = {
-            "message": f"Successfully created account for {new_user.username}"
+            "message": f"Successfully created account for {new_user.username}",
+            "token": guard.encode_jwt_token(new_user)
         }
         return jsonify(message)
     else:
         # REMOVE/CHANGE AT PRODUCTION
         return jsonify("Something went wrong. Origin: routes/user.py: signup()")
+
+
+# *****************************
+# TESTING ROUTES:
+# REMOVE/CHANGE AT PRODUCTION
+# *****************************
+@user_api.route("/protected")
+@fp.auth_required
+def protected():
+    # REMOVE/CHANGE AT PRODUCTION
+    """
+    A protected endpoint. The auth_required decorator will require a header
+    containing a valid JWT
+    .. example::
+       $ curl http://localhost:5000/protected -X GET \
+         -H "Authorization: Bearer <your_token>"
+    """
+    return jsonify(
+        message="protected endpoint (allowed user {})".format(
+            fp.current_user().username,
+        )
+    )
+
