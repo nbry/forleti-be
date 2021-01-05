798a3f0799eca8f689f9fde03219254499eabbb4
nbry <nwbryant@umich.edu>
2021-01-05 10:49:33 -0800

    Add basic CRUD methods for BlogPost model

--- models/blogpost.py
+++ models/blogpost.py
@@ -1,3 +1,5 @@
+from flask import jsonify
+
 from connect_models import db
 
 
@@ -13,4 +15,78 @@ class BlogPost(db.Model):
         db.ForeignKey("users.id", ondelete="cascade"),
         nullable=False)
 
-    user = db.relationship("User")
+    user = db.relationship("User", backref="blogposts", lazy=True)
+
+    # *****************************
+    # CRUD methods:
+    # *****************************
+
+    @classmethod
+    def find_blogpost_by_id(cls, blogpost_id: int):
+        """
+        Search database for blog post by id. Return blog post or 404 error.
+        """
+        return cls.query.get_or_404(blogpost_id)
+
+    @classmethod
+    def create_new_blogpost(cls, user_id, content, title, subtitle=None):
+        """
+        Create a new blog post. Return blogpost
+        """
+        new_blogpost = cls(user_id=user_id, content=content, title=title, subtitle=subtitle)
+        db.session.add(new_blogpost)
+
+        # noinspection PyBroadException
+        try:
+            db.session.commit()
+            return new_blogpost
+
+        except Exception:
+            db.session.rollback()
+            return None
+
+    @classmethod
+    def delete_blogpost(cls, blogpost_id: int):
+        """
+        Identify blog post by id. Delete if matching post is found.
+        """
+        blogpost = cls.find_blogpost_by_id(blogpost_id)
+
+        if blogpost:
+            # noinspection PyBroadException
+            try:
+                db.session.delete(blogpost)
+                db.session.commit()
+                return jsonify({"message": "Post deleted!"})
+            except Exception:
+                db.session.rollback()
+                return jsonify({"message": "Something went wrong."})
+        else:
+            return jsonify({"message": f"No post with id {blogpost_id} found"})
+
+    @classmethod
+    def update_blogpost(cls, blogpost_id, **kwargs):
+        """
+        Find a post by id. Looks for "title", "subtitle", or "content" within
+        kwargs and updates the blog post with the provided items.
+
+        e.g.
+        update_blogpost(3, title="New Dog", subtitle="So Excited!", age=9)
+        -> Finds blogpost with id of 3
+        -> Updates title to "New Dog"
+        -> Updates subtitle to "So Excited!"
+        -> age parameter is ignored.
+        """
+
+        blogpost = cls.find_blogpost_by_id(blogpost_id)
+        blogpost.title = kwargs.get("title", blogpost.title)
+        blogpost.subtitle = kwargs.get("subtitle", blogpost.subtitle)
+        blogpost.content = kwargs.get("content", blogpost.content)
+
+        # noinspection PyBroadException
+        try:
+            db.session.commit()
+            return jsonify({"message": "Post updated!"})
+        except Exception:
+            db.session.rollback()
+            return jsonify({"message": "Something went wrong."})
