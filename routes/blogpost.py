12a731031dd8554d64bf462ff22197c201e9caa6
nbry <nwbryant@umich.edu>
2021-01-04 16:38:32 -0800

    Relocate blogpost model into separate file from routes

--- routes/blogpost.py
+++ routes/blogpost.py
@@ -11,16 +11,3 @@ blogpost_api = Blueprint('blogpost_api', __name__)
 # CREATE AND PATCH A BLOG POST (GET AND POST)
 # EDIT A POST (PATCH)
 
-class BlogPost(db.Model):
-    __tablename__ = "blogposts"
-
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String(100), nullable=False)
-    subtitle = db.Column(db.String(100))
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(
-        db.Integer,
-        db.ForeignKey("users.id", ondelete="cascade"),
-        nullable=False)
-
-    user = db.relationship("User")
