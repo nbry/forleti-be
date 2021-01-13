e4b2436b256cdeb53632573d9844dfc459d7e78c
nbry <nwbryant@umich.edu>
2021-01-13 13:35:24 -0800

    Set up flask-cors to work with front end app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
+from flask_cors import CORS
 from project import create_app
 
 config_settings = os.environ.get("APP_CONFIG_SETTINGS", "development.cfg")
 
 app = create_app(config_settings)
+CORS(app)
 
 # # IF RUNNING ON PYCHARM:
 # if __name__ == '__main__':
--- project/routes/blogpost.py
+++ project/routes/blogpost.py
@@ -1,9 +1,53 @@
 """ Routes for blogpost-related tasks """
-from flask import Blueprint
+import flask_praetorian as fp
+from flask import jsonify, request
+from project.models import BlogPost, User
+from . import blogpost_api_blueprint
 
 
-# BLOG POST ROUTE BY ID (GET)
+@blogpost_api_blueprint.route('/bp/<int:blogpost_id>')
+def get_blogpost_by_id(blogpost_id):
+    """
+    Search for a blog post based on id.
+    If matching blog post found, return post.
+    """
+    res = BlogPost.query.get(blogpost_id)
+
+    if res:
+        blogpost = {
+            "title": res.title,
+            "subtitle": res.subtitle,
+            "content": res.content,
+            "author": res.user
+        }
+        return jsonify({"blogpost": blogpost})
+    else:
+        message = {"message": f"No blog post with id {blogpost_id} found"}
+        return jsonify(message)
+
+
+@blogpost_api_blueprint.route('/bp/new')
+@fp.auth_required
+def create_new_blog_post():
+    """
+    Create a new blog post
+    """
+    req = request.json
+
+
+#     TO BE FINISHED AFTER FRONT END WYSIWYG EDITOR IS CONFIGURED...
+
+
+@blogpost_api_blueprint.route('/<username>')
+def get_blogposts_by_username(username):
+    """
+    Search for a user by username.
+    If matching user is found, return all blog posts made by the user
+    """
+    res = User.lookup(username)
+    if res:
+        return jsonify({"posts": res.posts})
+
 # BLOG POSTS BY USER (GET)
 # CREATE AND PATCH A BLOG POST (GET AND POST)
 # EDIT A POST (PATCH)
-
