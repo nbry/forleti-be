54fe901889c66822e76b04386e9f02bd5fbcac42
nbry <nwbryant@umich.edu>
2021-01-17 16:12:22 -0800

    Delete style_settings and implement theme instead of colors

--- project/models/user_profile_settings.py
+++ project/models/user_profile_settings.py
@@ -10,30 +10,36 @@ class UserProfileSettings(db.Model):
     bio = db.Column(db.String(200))
     avatar_url = db.column(db.Text)
     header_url = db.column(db.Text)
+    theme = db.Column(db.Integer, default=1)
+    dark_mode = db.Column(db.Boolean, default=False)
     user_id = db.Column(
         db.Integer,
         db.ForeignKey("users.id", ondelete="cascade"),
-        nullable=False)
+        nullable=False,
+        unique=True)
 
     user = db.relationship("User", backref="profile_settings", lazy=True)
 
     @classmethod
-    def update_setting(cls, user_id, settings: dict):
+    def update_settings(cls, user_id, settings: dict):
         """
         Update profile settings provided with information provided
         in a dictionary. Settings dictionary can include bio,
-        avatar_url, and header_url. Intended to receive data from front
-        end after user submits a form to change settings. Returns a dictionary
-        or message.
+        avatar_url, header_url, theme, and/or dark_mode. Intended to receive
+        data from front end after user submits a form to change settings.
+        Returns a dictionary of user settings or message.
         """
         user_set = cls.query.filter_by(user_id=user_id).one_or_none()
 
         if user_set:
             # noinspection PyBroadException
             try:
+                # Not the DRYest code, but keep these explicit.
                 user_set.bio = settings.get("bio", user_set.bio)
                 user_set.avatar_url = settings.get("avatar_url", user_set.avatar_url)
                 user_set.header_url = settings.get("header_url", user_set.header_url)
+                user_set.theme = settings.get("theme", user_set.theme)
+                user_set.dark_mode = settings.get("dark_mode", user_set.dark_mode)
                 db.session.commit()
 
                 return user_set
