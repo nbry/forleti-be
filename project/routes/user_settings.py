188b7cb549c885b7865ac55568c5f061b626a7aa
nbry <nwbryant@umich.edu>
2021-01-19 12:07:47 -0800

    Comment out depracated route

--- project/routes/user_settings.py
+++ project/routes/user_settings.py
@@ -1,6 +1,6 @@
 """ Routes for user settings-related tasks """
 import flask_praetorian as fp
-from flask import jsonify, request
+from flask import  request
 
 from . import user_settings_api_blueprint
 from project.models import User
@@ -32,29 +32,29 @@ def change_user_setting():
 
 # This route is not being utilized at the moment, but keep it
 # for future implementation
-@user_settings_api_blueprint.route('/settings')
-@fp.auth_required
-def get_user_settings():
-    """
-    Get currently logged in user and return settings dictionary.
-    """
-
-    user = fp.current_user()
-
-    if user:
-        # noinspection PyBroadException
-        try:
-            settings = {
-                "username": user.username,
-                "email": user.email,
-                "bio": user.profile_settings[0].bio,
-                "avatar_url": user.profile_settings[0].avatar_url,
-                "header_url": user.profile_settings[0].header_url,
-                "theme": user.profile_settings[0].theme,
-                "dark_mode": user.profile_settings[0].dark_mode
-            }
-
-            return jsonify({"settings": settings})
-
-        except Exception:
-            return jsonify({"message": "Something went wrong."}), 400
+# @user_settings_api_blueprint.route('/settings')
+# @fp.auth_required
+# def get_user_settings():
+#     """
+#     Get currently logged in user and return settings dictionary.
+#     """
+#
+#     user = fp.current_user()
+#
+#     if user:
+#         # noinspection PyBroadException
+#         try:
+#             settings = {
+#                 "username": user.username,
+#                 "email": user.email,
+#                 "bio": user.profile_settings[0].bio,
+#                 "avatar_url": user.profile_settings[0].avatar_url,
+#                 "header_url": user.profile_settings[0].header_url,
+#                 "theme": user.profile_settings[0].theme,
+#                 "dark_mode": user.profile_settings[0].dark_mode
+#             }
+#
+#             return jsonify({"settings": settings})
+#
+#         except Exception:
+#             return jsonify({"message": "Something went wrong."}), 400
